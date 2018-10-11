935ea1b4d05e4003b9716289c33f88c42811faca
iZuf69z7zud21ifa7jenptZ <101.132.160.42>
2018-10-11 15:43:11 +0800

    version 1.0

---
+++ SpringBoot_Blog/src/test/java/com/ctyl/blog/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.ctyl.blog.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello")
+                .accept(MediaType.APPLICATION_JSON))
+           ;
+
+    }
+}
